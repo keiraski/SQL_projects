a6c0cff80c11fe1094695fc233ad11a7e567fa87
keiraski <keiraski@student.21-school.ru>
2025-06-29 20:24:35 +0300

    add day06

---
+++ SQL_beginner.Day06-2/src/ex02/day06_ex02.sql
@@ -0,0 +1,6 @@
+select p.name, m.pizza_name, m.price, (m.price - (m.price * pd.discount/100) ) as discount_price, p2.name as pizzeria_name
+from person_order inner join menu m on m.id = person_order.menu_id
+inner join person p on p.id = person_order.person_id
+inner join person_discounts pd on p.id = pd.person_id and pd.pizzeria_id = m.pizzeria_id
+inner join pizzeria p2 on m.pizzeria_id = p2.id
+order by 1,2;
---
+++ SQL_beginner.Day06-2/src/ex05/day06_ex05.sql
@@ -0,0 +1,5 @@
+comment on column person_discounts.id is 'primary key identifier';
+comment on column person_discounts.person_id is 'person identifier';
+comment on column person_discounts.pizzeria_id is 'pizzeria identifier';
+comment on column person_discounts.discount is 'discount for pair person and pizzeria';
+comment on table person_discounts is 'Discounts for persons';
